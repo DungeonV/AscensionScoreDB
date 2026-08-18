a20b7bf6089e21e38910d8733d9fc7a1cb8ff252
DungeonV <rockmarco97@gmail.com>
2026-08-18 02:03:36 +0200

    Auto-update database via secure Cloudflare Worker

--- ScoreDB.lua
+++ ScoreDB.lua
@@ -1,3 +1,13 @@
 
-AscensionScoreLogs = {
+AscensionScoreDB = {
+	["players"] = {
+		["Giulio-Vol'jin - Conquest of Azeroth"] = {
+			["lastUpdate"] = 1787010899,
+			["name"] = "Giulio",
+			["score"] = 0,
+			["realm"] = "Vol'jin - Conquest of Azeroth",
+		},
+	},
+	["runs"] = {
+	},
 }
